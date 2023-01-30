a03c1b8a733fba47da3955beb415f4bd927d41ac
alwi09 <alwi.irfani1927@gmail.com>
2023-01-30 14:03:51 +0700

    Add: Todolist struct and Response struct

--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -1,1 +1,14 @@
 package database
+
+type Todolist struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      bool   `json:"status"`
+}
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
